1ff4698a5e558056bf02442624a99ce6635784b7
AlexeyLepov <77492646+AlexeyLepov@users.noreply.github.com>
2022-12-24 20:03:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,20 +14,24 @@ class App(customtkinter.CTk):
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.geometry("1470x820")
 
-        # set grid layout 1x2
+        #############################
+        #    set grid layout 1x2    #
+        #############################
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
-        # load images with light and dark mode image
+        ####################################################
+        #    load images with light and dark mode image    #
+        ####################################################
         pathImages = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Assets/Images")
         pathIcons = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Assets/Icons")   
-        self.imageLogo = customtkinter.CTkImage(Image.open(os.path.join(pathIcons, "Leti_logo.png")), size=(32, 32))
-        self.imageERG = customtkinter.CTkImage(Image.open(os.path.join(pathImages, "BG_Barabasi_Albert.png")), size=(500, 300))
+        self.imageLogo = customtkinter.CTkImage(Image.open(os.path.join(pathIcons, "leti.png")), size=(32, 32))
+        self.menuImageERG = customtkinter.CTkImage(Image.open(os.path.join(pathIcons, "leti.png")), size=(24, 24))
+        self.menuImageBAG = customtkinter.CTkImage(Image.open(os.path.join(pathIcons, "leti.png")), size=(24, 24))
+        self.menuImageBRG = customtkinter.CTkImage(Image.open(os.path.join(pathIcons, "leti.png")), size=(24, 24))
+        self.imageERG = customtkinter.CTkImage(Image.open(os.path.join(pathImages, "BG_Erdos_Renyi.png")), size=(500, 300))
         self.imageBAG = customtkinter.CTkImage(Image.open(os.path.join(pathImages, "BG_Barabasi_Albert.png")), size=(500, 300))
-        self.imageBRG = customtkinter.CTkImage(Image.open(os.path.join(pathImages, "BG_Barabasi_Albert.png")), size=(500, 300))
-        self.menuImageERG = customtkinter.CTkImage(Image.open(os.path.join(pathImages, "BG_Barabasi_Albert.png")), size=(24, 24))
-        self.menuImageBAG = customtkinter.CTkImage(Image.open(os.path.join(pathImages, "BG_Barabasi_Albert.png")), size=(24, 24))
-        self.menuImageBRG = customtkinter.CTkImage(Image.open(os.path.join(pathImages, "BG_Barabasi_Albert.png")), size=(24, 24))
+        self.imageBRG = customtkinter.CTkImage(Image.open(os.path.join(pathImages, "BG_Bollobas_Riordan.png")), size=(500, 300))
 
         #################################
         #    create navigation frame    #
@@ -37,11 +41,11 @@ class App(customtkinter.CTk):
         self.navigation_frame.grid_rowconfigure(4, weight=1)
         self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="  Модели случайных графов", image=self.imageLogo, compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
-        self.MenuButtonERG = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Home", fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), image=self.imageERG, anchor="w", command=self.MenuButtonERG_event)
+        self.MenuButtonERG = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Home", fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), image=self.menuImageERG, anchor="w", command=self.MenuButtonERG_event)
         self.MenuButtonERG.grid(row=1, column=0, sticky="ew")
-        self.MenuButtonBAG = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Frame 2", fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), image=self.imageERG, anchor="w", command=self.MenuButtonBAG_event)
+        self.MenuButtonBAG = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Frame 2", fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), image=self.menuImageBAG, anchor="w", command=self.MenuButtonBAG_event)
         self.MenuButtonBAG.grid(row=2, column=0, sticky="ew")
-        self.MenuButtonBRG = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Frame 3", fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), image=self.imageERG, anchor="w", command=self.MenuButtonBRG_event)
+        self.MenuButtonBRG = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Frame 3", fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), image=self.menuImageBRG, anchor="w", command=self.MenuButtonBRG_event)
         self.MenuButtonBRG.grid(row=3, column=0, sticky="ew")
         self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"], command=self.change_appearance_mode_event)
         self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
